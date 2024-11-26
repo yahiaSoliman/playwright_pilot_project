aa29329b57faf8ddc4db0fb123784a2b4cc8fcaa
yahiaSoliman <67029767+yahiaSoliman@users.noreply.github.com>
2024-11-26 15:33:40 +0200

    Update test_git_actions.py

--- test_git_actions.py
+++ test_git_actions.py
@@ -3,7 +3,7 @@ class TestGitActions:
     age = 15
 
     def test_student_name(self):
-        assert self.name == "ahmed ali"
+        assert self.name == "ahmed nour"
 
     def test_student_age(self):
         assert self.age == 15
