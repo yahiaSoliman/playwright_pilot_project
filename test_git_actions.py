833fc801711d6ea7d4ef06402cd0664fdc9be86a
yahiaSoliman <67029767+yahiaSoliman@users.noreply.github.com>
2024-12-03 10:45:16 +0200

    add two files to YML

--- test_git_actions.py
+++ test_git_actions.py
@@ -3,7 +3,7 @@ class TestGitActions:
     age = 15
 
     def test_student_name(self):
-        assert self.name == "ahmed nour"
+        assert self.name == "ahmed ali"
 
     def test_student_age(self):
         assert self.age == 15
