04e4386c719b3b660971b1041d9ff144167daf2d
yahiaSoliman <67029767+yahiaSoliman@users.noreply.github.com>
2025-01-05 16:31:17 +0200

    update

--- pure_water/test_git_actions.py
+++ pure_water/test_git_actions.py
@@ -1,8 +1,14 @@
+import sys
+import os
+
+# Determine the parent directory and add it to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, parent_dir)
+
 from data import Data
 
 
 class TestGitActions:
-
     x = Data.change_owner_command_id_qa_stg
 
     def test_student_name(self):
